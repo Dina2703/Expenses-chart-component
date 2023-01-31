b3d4dadaa7227536d46f903d1e67db58f442f5ed
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-01-30 21:17:31 -0500

    json-data: displayed month names

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 fetch("./data.json")
   .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((data) => myFunc(data));
+
+const months = document.getElementById("months");
+
+function myFunc(data) {
+  data.forEach((item) => {
+    let el = document.createElement("div");
+    el.classList.add("month");
+    el.innerText = item.day;
+    months.appendChild(el);
+  });
+}
